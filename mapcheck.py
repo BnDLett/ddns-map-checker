d4a1f9db5b3ce72d38cf0a6d4af9e1c24e87a307
Hatsune Miku <73402249+Weebed-Coder@users.noreply.github.com>
2023-01-09 20:21:48 -0500

    Delete mapcheck.py

--- mapcheck.py
+++
@@ -1,8 +0,0 @@
-import websocket, time
-
-while True:
-    ws = websocket.WebSocket()
-    ws.connect('https://mdnmapcheck.ddns.net', header=["User-Agent: MyProgram", "x-custom: header"])
-    ws.send("Test")
-    print(ws.recv())
-    time.sleep(5)
